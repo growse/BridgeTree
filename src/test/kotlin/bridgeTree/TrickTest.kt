73c7c443206f243b414fa52924b41ec46a4201e9
growse <github@growse.com>
2018-09-20 20:02:11 +0100

    Test that incomplete hand throws exception

--- src/test/kotlin/bridgeTree/TrickTest.kt
+++ src/test/kotlin/bridgeTree/TrickTest.kt
@@ -1,6 +1,7 @@
 package bridgeTree
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 
 class TrickTest {
@@ -14,5 +15,16 @@ class TrickTest {
         assertEquals(Card(Suit.DIAMONDS, Pip.TEN), trick.getWinningCard().card)
         assertEquals(Place.SOUTH, trick.getWinningCard().player.place)
 
+    }
+
+    @Test
+    fun tryingToGetTheWinningTrickShouldFailWhenNotEnoughCards() {
+        val trick = Trick()
+        trick.addCard(Player(Place.NORTH, Hand(listOf())), Card(Suit.DIAMONDS, Pip.SEVEN))
+        trick.addCard(Player(Place.EAST, Hand(listOf())), Card(Suit.HEARTS, Pip.TWO))
+        trick.addCard(Player(Place.SOUTH, Hand(listOf())), Card(Suit.DIAMONDS, Pip.TEN))
+        assertThrows(IncompleteHandException::class.java) {
+            trick.getWinningCard()
+        }
     }
 }
